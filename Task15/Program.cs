17c2b7432394b94f2769305b7802213e43a6b8c3
Sergey Gukov <svgukoff@mail.ru>
2022-08-26 11:24:41 +0300

    add folder Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число обозначающее день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+bool findWeekEnd(int weekDay)
+{
+    bool numberDay;
+    if ((weekDay == 6) ^ (weekDay == 7))
+    {
+        numberDay = true;
+    }
+    else
+    {
+        numberDay = false;
+    }
+    return numberDay;
+}
+
+if (findWeekEnd(day) == true)
+{
+    Console.WriteLine("Это выходной день недели");
+}
+else
+{
+    Console.WriteLine("Это рабочий день недели");
+}
