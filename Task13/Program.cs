7d1ccd6a18bf88c5c2385c2e782897ac5454664f
Sergey Gukov <svgukoff@mail.ru>
2022-08-26 10:49:12 +0300

    add folder Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿int findThirdDigit(int Number)
+{
+    while (Number / 100 > 9)
+    {
+        Number = Number / 10;
+    }
+    int thirdDigit = Number%10;
+    return thirdDigit;
+}
+
+int result = -1;
+
+Console.Write("Введите число - ");
+int newNumber = Convert.ToInt32(Console.ReadLine());
+
+if (newNumber / 100 > 0)
+{
+    result = findThirdDigit(newNumber);
+    Console.Write("Третья цифра - ");
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
