c7fca95481a2ccb8d35c6ae5720a4b2b02794c0c
Sergey Gukov <svgukoff@mail.ru>
2022-08-26 10:31:54 +0300

    add folder Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(100, 1000);
+
+Console.WriteLine(randomNumber);
+
+int secondNumber = (randomNumber / 10)% 10;
+
+Console.WriteLine(secondNumber);
